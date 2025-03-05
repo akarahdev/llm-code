c9eda8ac06a246c34846733960f22a4a4c04bc78
akarahdev <endisticbutitsforgit@gmail.com>
2025-03-04 22:24:26 -0500

    feat: add simple manager AI

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 use std::sync::LazyLock;
 
 use anyhow::Result;
-use base::BaseAI;
-use openai::chat::{ChatCompletionMessage, ChatCompletionMessageRole};
+use manager::ManagerAI;
 use rust_dotenv::dotenv::DotEnv;
+use tokio::io::AsyncReadExt;
 
 pub static ENV: LazyLock<DotEnv> = LazyLock::new(|| DotEnv::new(""));
 
 mod base;
+mod manager;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut ai = BaseAI::new();
-    ai.add_message(ChatCompletionMessage {
-        role: ChatCompletionMessageRole::System,
-        content: Some("hi".to_string()),
-        ..Default::default()
-    });
+    let mut manager = ManagerAI::new();
     loop {
-        ai.add_message(ChatCompletionMessage {
-            role: ChatCompletionMessageRole::User,
-            content: Some("This is a user's message for testing, say hi!".into()),
-            ..Default::default()
-        });
-        let response = ai.get_response().await;
-        println!("{:#?}", response);
+        let mut buf = Vec::new();
+        loop {
+            let mut sbuf = [0];
+            let _ = tokio::io::stdin().read(&mut sbuf).await.unwrap();
+            if sbuf[0] == b'\n' {
+                break;
+            } else {
+                buf.push(sbuf[0]);
+            }
+        }
+        let str = String::from_utf8(buf).unwrap();
+        manager.orchestrate(str).await;
     }
 }
